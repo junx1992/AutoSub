8a83a6b8eaa0aa5be106ca9395574c34d9f7edba
junx1992 <junx1992@gmail.com>
2012-11-30 10:00:12 +0800

    delete the origin

--- src/autosub.py
+++ src/autosub.py
@@ -1,37 +1,149 @@
+"""
+Get the Main Frame and link the other
+"""
+
 import core.ffmpeg_decoder as fd
 import core.sub_generator as sg
 import core.spectrum as sp
-from core.naive_vad2 import *
-import VLC.vlc_wx as vlc_wx
 import sys
 import wx
+from core.naive_vad2 import *
+from VLC.playerpart import *
+from VLC.spectrum_widget.SpecWin import *
+from VLC.subtitle_widget.SubtitleEditor import *
+sys.path.append("/VLC")
 
-
-class MainFrame(vlc_wx.MyFrame):
+class MainFrame(wx.Frame):
+    """ The Main Window"""
     def __init__(self,title):
-        fram=vlc_wx.MyFrame.__init__(self,title)
-        self.subtitle=None
-        self.ohandle=None
-        # variable
-        self.end=0
+        frame=wx.Frame.__init__(self,None,-1,title)
 
         
-    def OnOpen(self, evt):
-        super(MainFrame, self).OnOpen(self)
+        # Attribute
+        myfont=wx.Font(11, wx.SWISS, wx.NORMAL, wx.NORMAL,False, u'Segoe UI')
+        Backgroud=(57,59,66)
+        Fontcolor=(229,229,229)
+        bback=(77,77,77)
+        ctrlpanelcolor=(51,51,51)
+        self.mainpanel=wx.Panel(self)
+        self.playerpanel=PlayerPanel(self.mainpanel)
+        self.subpanel=Subtitle(self.mainpanel,-1)
+        self.spec=SpecPanel(self.mainpanel,"VLC/spectrum_widget/Icon/speceg.jpg")
+        self.splitter = wx.SplitterWindow(self.mainpanel, -1, style=wx.SP_3D)
+        self.subtitle=None
+        self.ohandle=None
+        self.end=0
+        # Menu
+        #Menu Bar
+        self.frame_menubar=wx.MenuBar()
+        #  File Menu
+        self.icon=wx.Icon('./VLC/Icons/autosub.ico',wx.wx.BITMAP_TYPE_ICO);
+        self.SetIcon(self.icon);
+        self.file_menu=wx.Menu()
+        menu_open=self.file_menu.Append(-1,"&Open \tCtrl+O")
+        self.file_menu.AppendSeparator()
+        menu_close=self.file_menu.Append(-1,"&Close \tCtrl+C")
+        self.Bind(wx.EVT_MENU, self.OnOpen, menu_open)
+        self.Bind(wx.EVT_MENU, self.OnExit, menu_close)
+        self.frame_menubar.Append(self.file_menu, "&File")
+
+
+        #  Edit Menu
+        self.edit_menu=wx.Menu()
+        menu_play=self.edit_menu.Append(-1,"&Play \tCtrl+P")
+        self.edit_menu.AppendSeparator()
+        menu_pause=self.edit_menu.Append(-1,"P&ause \tCtrl+A")
+        self.edit_menu.AppendSeparator()
+        menu_stop=self.edit_menu.Append(-1,"&Stop \tCtrl+S")
+        self.edit_menu.AppendSeparator()
+        menu_fullscreen=self.edit_menu.Append(-1,"&FullScreen \tCtrl+F")
+        self.edit_menu.AppendSeparator()
+        menu_volume=self.edit_menu.Append(-1,"&Volume \tCtrl+V")
+        self.edit_menu.AppendSeparator()
+
+        self.frame_menubar.Append(self.edit_menu, "&Edit")
+
+        #  Audio Menu
+        self.sub=wx.Menu()
+        op=self.sub.Append(-1,"Open Subtitle")
+        self.sub.AppendSeparator()
+        sa=self.sub.Append(-1,"Save Subtitle")
+        self.frame_menubar.Append(self.sub, "Subtitle")
+        #  Help Menu
+        self.help_menu=wx.Menu()                
+        menu_feedback=self.help_menu.Append(-1,"&FeedBack")
+        self.help_menu.AppendSeparator()
+        self.frame_menubar.Append(self.help_menu, "Help")
+        self.SetMenuBar(self.frame_menubar)
+        
+        # Layout
+        self.__DoLayout__()
+        self.mainpanel.SetBackgroundColour(wx.BLACK)
+
+        # Bind Event
+        #  Menu Bind
+        self.Bind(wx.EVT_MENU, self.OnPlayButton, menu_open)
+        self.Bind(wx.EVT_MENU, self.playerpanel.OnExit, menu_close)
+        self.Bind(wx.EVT_MENU, self.OnPlayButton, menu_play)
+        self.Bind(wx.EVT_MENU, self.playerpanel.OnPause, menu_pause)
+        self.Bind(wx.EVT_MENU, self.playerpanel.OnStop, menu_stop)
+        self.Bind(wx.EVT_MENU, self.OnToggleFullScreen, menu_fullscreen)
+        self.Bind(wx.EVT_MENU, self.playerpanel.OnToggleVolume, menu_volume)
+        self.Bind(wx.EVT_MENU, self.playerpanel.OnFeedBack, menu_feedback)
+        self.Bind(wx.EVT_MENU, self.subpanel.OpenFile, op)
+        self.Bind(wx.EVT_MENU, self.SaveSubtitle, sa)
+        
+        self.subpanel.Bind(wx.EVT_LISTBOX_DCLICK,self.Select,self.subpanel.listBox)
+        self.Bind(wx.EVT_BUTTON,self.OnToggleFullScreen,self.playerpanel.fullscreen)
+        self.playerpanel.Bind(wx.EVT_BUTTON,self.OnPlayButton,self.playerpanel.play)
+        self.playerpanel.Bind(wx.EVT_TIMER, self.OnTimer, self.playerpanel.timer)
+        self.Bind(wx.EVT_CLOSE,self.OnExit)
+
+    def __DoLayout__(self):    
+
+        BigSizer=wx.GridBagSizer(hgap=0,vgap=0)
+        BigSizer.Add(self.subpanel,(0,0),flag=wx.EXPAND)
+        BigSizer.Add(self.spec,(1,0),flag=wx.EXPAND)
+        BigSizer.Add(self.splitter,(0,1),(2,1))
+        BigSizer.Add(self.playerpanel,(0,2),(2,1),flag=wx.EXPAND)
+
+        BigSizer.AddGrowableRow(0,4)
+        BigSizer.AddGrowableRow(1,1)
+        BigSizer.AddGrowableCol(0,1)        
+        BigSizer.AddGrowableCol(2,2)
+        self.mainpanel.SetSizer(BigSizer)
+        self.SetMinSize((1050,800))
+
+    def Select(self,evt):
+        self.subpanel.ChooseOneItem(self.subpanel)
+        begintime=self.subpanel.begintime.GetValue()
+        endtime=self.subpanel.endtime.GetValue()                
+        tmp_begin=begintime.split(':')
+        tmp_end=endtime.split(':')
+        if(len(tmp_begin)==3):
+                begin_sec=int(tmp_begin[0])*60*60+int(tmp_begin[1])*60+float(tmp_begin[2])
+                self.spec.GetLeftLex(self.spec,begin_sec)
+        if(len(tmp_end)==3):
+                end_sec=int(tmp_end[0])*60*60+int(tmp_end[1])*60+float(tmp_end[2])
+                self.spec.GetRightLex(self.spec,end_sec)
+
+    def OnOpen(self,evt):        
+        self.playerpanel.OnOpen(None)
+        self.SetTitle("%s - AutoSub" % self.playerpanel.title)
         lan={"English":"en" ,"Chinese":"zh-cn" ,"Japanese":"ja"}
         lang_from = None
         lang_to = None
         source = None
         target = None
-        if self.select_dialog.isrecognize==True:            
+        if self.playerpanel.select_dialog.isrecognize==True:            
             # Set recognize parameter
-            lang_from=lan[self.select_dialog.sorcelan]
+            lang_from=lan[self.playerpanel.select_dialog.sorcelan]
             
-        if self.select_dialog.istranslate==True:
+        if self.playerpanel.select_dialog.istranslate==True:
             # Set translation parameter
-            lang_to=lan[self.select_dialog.targetlan]
+            lang_to=lan[self.playerpanel.select_dialog.targetlan]
 
-        source=self.mediapath
+        source=self.playerpanel.mediapath
         # Set target name
         if not target:
             target = source[:source.rfind('.')] + '.srt'
@@ -51,10 +163,31 @@ class MainFrame(vlc_wx.MyFrame):
         vad.start()
         sub.start()
         spec.start()
-        self.Spec.OpenData(self.Spec,handle)
-        
+        self.spec.OpenData(self.spec,handle)
 
-    # float second to time string 
+    def SaveSubtitle(self,evt):
+        self.subpanel.SaveFile(self.subpanel)
+        self.playerpanel.player.video_set_subtitle_file(self.subpanel.filename)
+        
+    def OnPlayButton(self,evt):
+        pausebmp=wx.Bitmap("./Icons/pause.png",wx.BITMAP_TYPE_PNG)
+        pausebmp.SetSize(size=(40,40))
+        playbmp=wx.Bitmap("./Icons/play.png",wx.BITMAP_TYPE_PNG)                
+        playbmp.SetSize(size=(40,40))
+        
+                
+        if not self.playerpanel.player.get_media():
+                self.OnOpen(None)
+                self.playerpanel.play.SetBitmap(bmp=pausebmp)
+        else:
+                self.playerpanel.OnPause(self)
+                if self.playerpanel.player.is_playing()==True:                                                           
+                        self.playerpanel.play.SetBitmap(bmp=playbmp)
+                                
+                else:                                
+                        self.playerpanel.play.SetBitmap(bmp=pausebmp)
+
+     # float second to time string 
     def OnFormatTime(self,floattime):
         time=int(floattime)
         str_decimaltime=str(int((floattime-time)*1000))
@@ -79,23 +212,40 @@ class MainFrame(vlc_wx.MyFrame):
         return string_time
 
     def OnTimer(self,evt):
-        super(MainFrame, self).OnTimer(self)        
+        self.playerpanel.OnTimer(self.playerpanel)
+        time = self.playerpanel.player.get_time()
+        
+        #give the time to spec
+        self.spec.CurrPos(self.spec,time)
+        #spectoright
+        ll=self.spec.GetLeft(self.spec)
+                
+        if(ll!="-1"):
+                self.subpanel.SetLeft(self.subpanel,ll)
+        rr=self.spec.GetRight(self.spec)
+        if(rr!="-1"):
+                self.subpanel.SetRight(self.subpanel,rr)
+        
         if self.ohandle.has_data(1):            
             (start,self.end,text)=self.ohandle.read(1)[2][0][0]            
-            self.player.video_set_subtitle_file(self.subtitle)
+            self.playerpanel.player.video_set_subtitle_file(self.subtitle)
             str_start=self.OnFormatTime(start)
             str_end=self.OnFormatTime(self.end)
             self.subpanel.AddSub(self.subpanel,str_start,str_end,text)
             # Set buffer time
-        if self.player.get_length()!=0:
-            self.buffergauge.SetValue(self.end*self.buffergauge.GetRange()*1000/self.player.get_length())        
-        # Link with subtitle
-        #self.subpanel.OpenFile(self.subtitle)
+        if self.playerpanel.player.get_length()!=0:
+            self.playerpanel.buffergauge.SetValue(self.end*self.playerpanel.buffergauge.GetRange()*1000/self.playerpanel.player.get_length())
+    def OnExit(self, evt):
+        self.playerpanel.player.stop()
+        self.Close()
+        evt.Skip()
 
-    
+    def OnToggleFullScreen(self, evt):
+        is_fullscreen=self.IsFullScreen()
+        self.ShowFullScreen(show= not is_fullscreen)
 
- 
-        
+
+
 if __name__ == '__main__':
         # Create a wx.App(), which handles the windowing system event loop
         app = wx.PySimpleApp()
@@ -108,4 +258,3 @@ if __name__ == '__main__':
         PlayerFrame.Centre()
         PlayerFrame.Show()
         app.MainLoop()
-    
